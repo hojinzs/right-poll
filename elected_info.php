2b6b02f116a35bcebeacb78bb1ad7c24b7452adc
Steve Lee <lenscat@SteveLees-MacBook-Pro.local>
2017-05-05 13:20:01 +0900

    gg

---
+++ elected_info.php
@@ -0,0 +1,74 @@
+<HTML>
+<HEAD>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+  <link rel="stylesheet" href="/style/site.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+  <title>직위 :: 당선자명</title>
+</HEAD>
+<BODY>
+
+  <div class="container">
+
+    <header class="row">
+      <div class="col-md-2">
+        <img src="http://placehold.it/160x160">
+      </div>
+      <div class="col-md-10">
+        <h1 id="pagetitle">당선자명<br></h1>
+        <p>직위</p>
+      </div>
+    </header>
+
+    <nav>
+      <ul>
+        <il><a href="/">홈</a></il>
+        |
+        <il>ABOUT US</il>
+      </ul>
+    </nav>
+
+    <div id="policy_list">
+      <div class="panel panel-default">
+        <div class="panel-body">
+          <p>공약 카테고리1</p>
+          <p>설명</p>
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-body">
+          <div class="row">
+            <div class="col-md-8">
+              <p>공약 카테고리2</p>
+              <p>설명</p>
+            </div>
+            <div class="col-md-4">
+              <p>이행률</p>
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="min-width: 2em;">0%</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <ul class="list-group">
+          <li class="list-group-item">공약1</li>
+          <li class="list-group-item">공약2</li>
+          <li class="list-group-item">공약3</li>
+          <li class="list-group-item">공약4</li>
+          <li class="list-group-item">공약5</li>
+        </ul>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-body">
+          <p>공약 카테고리3</p>
+          <p>설명</p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <footer>
+  </footer>
+</BODY>
+</HTML>
