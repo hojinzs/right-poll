96228d943053f23470570c803607492b93bd4683
Steve Lee <hojinzs@gmail.com>
2017-05-24 13:39:33 +0900

    헤더, 네비게이션바 조정

--- www/new_nav.php
+++ www/new_nav.php
@@ -1,10 +1,12 @@
 <nav>
     <div class="container">
-        <div class="brand">
-            공약 지킴이
-        </div>
-        <div class="nav-right">
-            <a href="/about.php" >ABOUT</a>
+        <div class="row">
+            <div class="brand">
+                공약 지킴이
+            </div>
+            <div class="nav-right">
+                <a href="/about.php" >ABOUT</a>
+            </div>
         </div>
     </div>
 </nav>
