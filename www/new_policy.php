03c7734f07b8f6ac3269976cd71096e34e6b9516
Steve Lee <hojinzs@gmail.com>
2017-05-28 22:58:47 +0900

    new layout: 공약정보 페이지 파라미터 메뉴 분기 추가

--- www/new_policy.php
+++ www/new_policy.php
@@ -2,6 +2,7 @@
 
 <?php
 $policy = \App\Common::getPolicyInfo($_GET['pol']);
+$menu = $_GET['mnu'];
 
 $elected = \App\Common::getElectedInfo($policy['elected_id']);
 $plans = \App\Common::getPlanList($policy['id']);
@@ -22,14 +23,10 @@ $title = "공약정보".$elected['chair']."-".$elected['name'];
         <div class="col-md-10">
             <div class="row">
                     <div class="plan_info col-md-12 col-xs-12">
-                        <button type="button" class="btn" onclick="location.href='/elected_info.php?id=<?php echo $policy['elected_id'] ?>'"><span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span>
-          공약 목록</button>
-                        <hr>
                         <span class="label label-primary"><?php echo $policy['label']; ?></span>
                         <h2><?php echo $policy['title']?></h2>
                         <button type="button" class="btn btn-primary" id="set_thumbsup" value="<?php echo $policy['id'] ?>"><span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span>
     좋아요</button> <?php echo $policy['likesum'] ?>명이 이 공약을 좋아합니다.
-                        <hr>
     <?php include 'planlist.php' ?>
                     </div>
                 <!-- <div class="col-md-12 col-xs-12"> -->
@@ -40,11 +37,25 @@ $title = "공약정보".$elected['chair']."-".$elected['name'];
                 </ul>
                 <!-- </div> -->
                 <div class="wr_contents col-md-12 col-xs-12">
-                    <div>
-                        <?php $tg="pol"; $prt_cmt=null?>
-                        <?php include 'new_cmt_summit.php' ?>
-                        <?php include 'new_cmt_lst.php' ?>
-                    </div>
+<?php
+
+switch ($menu) {
+    case 'comment':
+        # 코멘트 페이지를 출력
+
+        include 'new_policy_cmt.php';
+
+        break;
+
+    default:
+        # 기본: 코멘트 페이지를 출력
+
+        include 'new_policy_cmt.php';
+        
+        break;
+}
+
+?>
                 </div>
             </div>
         </div>
---
+++ www/new_policy_cmt.php
@@ -0,0 +1,5 @@
+<div>
+    <?php $tg="pol"; $prt_cmt=null?>
+    <?php include 'new_cmt_summit.php' ?>
+    <?php include 'new_cmt_lst.php' ?>
+</div>
