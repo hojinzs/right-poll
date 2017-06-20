4c6eb3118c726afcee29c32eecf9c0b8fe6a1ade
Unknown <hojinzs@gmail.com>
2017-06-21 06:47:52 +0900

    달님 얼굴 좀 더 크게
    - 수정

--- www/new_states.php
+++ www/new_states.php
@@ -34,10 +34,10 @@ $og['img'] = $elected['profile'];
 
 <div class="container contents-box">
     <div class="row">
-        <div class="col-md-2">
+        <div class="col-md-3">
             <?php include 'new_elected_menu.php' ?>
         </div>
-        <div class="col-md-10">
+        <div class="col-md-9">
             <div class="row">
                 <div class="wr_contents col-md-12">
                     <?php
