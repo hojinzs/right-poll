24dd2d339cf5d4c2608da9e6d1860ab65b555a21
hojinzs <hojinzs@gmail.com>
2017-06-30 01:14:42 +0900

    Comment form 효율화2
    그냥

--- www/new_cmt_submit.php
+++ www/new_cmt_submit.php
@@ -4,7 +4,7 @@
 /**
  * 코멘트 작성 폼.
  * @var var $for 댓글이 달리는 곳 (elct,pol,...)
- * @var int $prt_cmt 상위 코멘트 ID
+ * @var int $prt_cmt 상위 코멘트 ID (기본 NULL)
  * @var var $cmt_target 코멘트 폼 제출시, 폼 식별자
  * $for = elct일 경우, $elected(당선자 정보)필요
  * $for = pol일 경우, $policy(공약 정보)
@@ -35,13 +35,13 @@ switch ($for) {
     <form name="comment_form" id="comment_form_<?=$cmt_target?>">
         <input type="hidden" id="target" name="target" value="<?=$target_type?>">
         <input type="hidden" id="target_id" name="target_id" value="<?=$target_id?>">
-        <input type="hidden" id="parents" name="parents" value="<?php echo $prt_cmt; ?>">
+        <input type="hidden" id="parents" name="parents" value="<?=$prt_cmt; ?>">
         <div class="wr_form_comment col-md-10 col-xs-12">
             <label for="nickname" class="sr-only">닉네임</label>
             <input id="nickname" class="wr_nick_input" name="nickname" placeholder="닉네임" value="<?=$_SESSION['user_nick']?>">
             <div class="wr_user_ip go_right">
                 <span class="glyphicon glyphicon-screenshot" aria-hidden="true"></span>
-                <?php echo $_SESSION['ip'];?>
+                <?=$_SESSION['ip'];?>
             </div>
             <label for="content" class="sr-only">한마디 내용</label>
             <textarea type="text" id="content" name="content" class="wr_comment_input" rows="4" resize="none" placeholder="나의 한마디"></textarea>
