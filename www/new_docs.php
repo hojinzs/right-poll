29406e843c1260a8f89f121937b2e16e4d048089
hojinzs <hojinzs@gmail.com>
2017-06-30 00:08:40 +0900

    docs.php

--- www/new_docs.php
+++ www/new_docs.php
@@ -1,5 +1,8 @@
 <?php require_once __DIR__ . '/../core/init.php';
 
+// 마크다운 라이브러리 사용
+use Michelf\MarkdownExtra;
+
 // 기본 메타데이터 (타이틀, 설명) 세팅
 $title = "실시간 공약이행률";
 $desc = "여러분의 도움이 필요합니다.";
@@ -9,29 +12,31 @@ $file_name = $_GET['name'];
 $file_type = $_GET['type'];
 $file_dir = "./docs/".$file_name.".".$file_type;
 
-// 파일 타입에 따라 처리 방법 변경 (작업 예정)
-// switch ($file_type) {
-//     case 'md':
-//         # 마크다운일 경우, HTML로 변환함.
-//         $file = file_get_contents($file_dir);
-//         use Michelf\MarkdownExtra;
-//         $html = MarkdownExtra::defaultTransform($file);
-//         break;
-//
-//     case 'html':
-//         # HTML일 경우, 그대로 뿌려줌.
-//         $file = file_get_contents($file_dir);
-//         $html = $file;
-//         break;
-//
-//     default:
-//         # code...
-//         break;
-// }
+// 파일 불러오기
+@$file = file_get_contents($file_dir);
 
-$text = file_get_contents($file_dir);
-use Michelf\MarkdownExtra;
-$html = MarkdownExtra::defaultTransform($text);
+if($file!=""){
+$html = "에러가 발생하였습니다.";
+}
+
+switch ($file_type) {
+   case 'md':
+       # 마크다운일 경우, HTML로 변환함.
+       $html = MarkdownExtra::defaultTransform($file);
+       break;
+
+   case 'html':
+       # HTML일 경우, 그대로 뿌려줌.
+       $html = $file;
+       break;
+
+   default:
+       # 인식하지 못하는 파일일 경우 ERROR.
+       $html = "알 수 없는 형식의 파일입니다.";
+       break;
+   }
+
+// 파일 타입에 따라 처리 방법 변경 (작업 예정)
 
 
 //오픈그래프 데이터 세팅 (head.php에서 사용)
