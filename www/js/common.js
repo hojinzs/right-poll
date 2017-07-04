c788c009fbc33ba7b24c99bf59b44eaf8c2ed9a0
Steve Lee <hojinzs@gmail.com>
2017-07-05 00:46:08 +0900

    댓글작성 메시지 수정

--- www/js/common.js
+++ www/js/common.js
@@ -70,7 +70,7 @@ $(document).ready(function(){
                 }
             },
             error:function(request,status,error){
-                alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+                // alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
                 alert("실패하였습니다");
             },
         })
