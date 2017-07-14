62a38665f8a1eb450b5bf1673a4c7f37a8144437
Unknown <hojinzs@gmail.com>
2017-07-15 00:08:30 +0900

    form CSS 수정

--- www/new_register.php
+++ www/new_register.php
@@ -25,31 +25,36 @@ $title = "공약지킴이::마이페이지";
 <div class="container">
     <div class="row">
         <div class="wr_contents col-md-12">
+            <div class="wr_single_wrapper">
 
             <h1>회원가입</h1>
 
-            <form class="wr_form">
+                <form class="wr_form">
 
-                <div class="group_btn">
-                    <input class="wr_form_input green" name="email" type="email" placeholder="이메일" disabled></input>
-                    <input class="wr_form_btn wr_btn wr_btn_blue green" name="email_send" type="button" value="인증코드 발송" disabled></input>
-                    <i class="fa fa-check green" aria-hidden="true"></i>
-                </div>
+                    <div class="group_btn green">
+                        <input class="wr_form_input" name="email" type="email" placeholder="이메일" disabled></input>
+                        <input class="wr_form_btn wr_btn wr_btn_blue" name="email_send" type="button" value="코드발송" disabled></input>
+                        <i class="fa fa-check" aria-hidden="true"></i>
+                    </div>
 
-                <div class="group_btn">
-                    <input class="wr_form_input" name="email_check" type="text" placeholder="인증코드 입력(4자리)" maxlength="4"></input>
-                    <input class="wr_form_btn wr_btn wr_btn_blue" name="email_send" type="button" value="인증코드 확인"></input>
-                </div>
+                    <div class="group_btn">
+                        <input class="wr_form_input" name="email_check" type="text" placeholder="인증코드 입력(4자리)" maxlength="4"></input>
+                        <input class="wr_form_btn wr_btn wr_btn_blue" name="email_send" type="button" value="확인"></input>
+                        <i class="fa fa-check" aria-hidden="true"></i>
+                    </div>
 
-                <div class="group_btn">
-                    <input class="wr_form_input" name="nick" type="text" placeholder="닉네임"></input>
-                    <input class="wr_form_btn wr_btn wr_btn_blue" name="nick_check" type="button" value="닉네임 확인" ></input>
-                </div>
+                    <div class="group_btn">
+                        <input class="wr_form_input" name="nick" type="text" placeholder="닉네임"></input>
+                        <input class="wr_form_btn wr_btn wr_btn_blue" name="nick_check" type="button" value="중복확인" ></input>
+                        <i class="fa fa-check" aria-hidden="true"></i>
+                    </div>
 
-                <input class="wr_form_input" name="password" type="password" placeholder="비밀번호"></input>
-                <input class="wr_form_input" name="password_again" type="password" placeholder="비밀번호 확인"></input>
-                <input class="wr_form_btn wr_btn wr_btn_blue" name="submit" type="submit" value="양식 제출 >"></input>
-            </form>
+                    <input class="wr_form_input" name="password" type="password" placeholder="비밀번호"></input>
+                    <input class="wr_form_input" name="password_again" type="password" placeholder="비밀번호 확인"></input>
+                    <input class="wr_form_btn wr_btn wr_btn_blue" name="submit" type="submit" value="양식 제출 >"></input>
+                </form>
+
+            </div>
 
         </div>
     </div>
