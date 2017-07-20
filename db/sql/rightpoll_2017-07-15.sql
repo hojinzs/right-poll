b830ffd227fd906cc710d37a76335615b4ea5f92
lenscat <hojinzs@gmail.com>
2017-07-20 18:10:50 +0900

    [DB] user login
    - edit table 'user'
    - create table 'log_login'

--- db/sql/rightpoll_2017-07-15.sql
+++ db/sql/rightpoll_2017-07-15.sql
@@ -5,12 +5,13 @@ DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `user` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `email` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `nick` varchar(12) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user_id` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `nick` varchar(12) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `modified_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `nick` (`nick`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `nick` (`nick`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
---
+++ db/sql/rightpoll_2017-07-20.sql
@@ -0,0 +1,12 @@
+# Dump of table user
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `log_login`;
+
+CREATE TABLE `log_login` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_idx` varchar(20) DEFAULT NULL,
+  `ip` varchar(16) DEFAULT '',
+  `login_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
